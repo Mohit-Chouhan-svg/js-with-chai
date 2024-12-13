0bc8c072d8ff998d139948838f136a5bc4b44ef1
Mohit-Chouhan-svg <chouhanmohit247@gmail.com>
2024-12-13 14:40:58 +0000

    conversionofdatatype.js

---
+++ 01_basic/conversionofdatatype.js
@@ -0,0 +1,21 @@
+let score = 33;
+console.log(typeof score);
+
+let change = String(score);
+console.log(change);
+console.log(typeof change);
+
+let inLogged = Boolean(score);
+console.log(inLogged);
+console.log(typeof inLogged);
+
+let str1 = "hello";
+let str2 = " mohit";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+

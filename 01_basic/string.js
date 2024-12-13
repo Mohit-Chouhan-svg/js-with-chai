ee2f8a83d09fecb9290af5277c1136bd50e3d818
Mohit-Chouhan-svg <chouhanmohit247@gmail.com>
2024-12-13 17:34:39 +0000

    string

---
+++ 01_basic/string.js
@@ -0,0 +1,14 @@
+const str1 = "mohit"
+console.log(str1);
+
+const string = new String('mohit-chouhan')
+console.log(string.substring(0, 4));
+console.log(string.slice(2, 4));
+console.log(string.charAt(2));
+console.log(string.indexOf('t'));
+
+const onestr = "   mohit   "
+console.log(onestr.trim);
+
+console.log(string.split('-'));
+

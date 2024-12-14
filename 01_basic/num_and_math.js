7aa51f64e2b7c63376ae1de2cfffb0ec48021d39
Mohit-Chouhan-svg <chouhanmohit247@gmail.com>
2024-12-14 15:17:13 +0000

    num_and_math

---
+++ 01_basic/num_and_math.js
@@ -0,0 +1,19 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance.toString);
+
+const num = 23.489
+console.log(num.toFixed(2));
+console.log(num.toPrecision(3));
+//console.log(num.toLocaleString);
+
+//************************Math**************************** */
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.6));
+
+console.log(Math.floor(Math.random * 10 + 1));

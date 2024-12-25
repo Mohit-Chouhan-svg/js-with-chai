be1053ea6e929c77f966796fbf381f5f2d18778d
Mohit-Chouhan-svg <chouhanmohit247@gmail.com>
2024-12-25 13:14:06 +0000

    Object3.js

---
+++ 02_basic/Object3.js
@@ -0,0 +1,14 @@
+const course = {
+    coursename: "js-in-hindi",
+    courseInstructor: "Hitesh",
+    price: "999"
+}
+
+const {courseInstructor: Instructor} = course
+console.log(Instructor);
+
+//Json API
+// {
+//     "name": "mohit",
+//     "course": "js-in-hindi"
+// }

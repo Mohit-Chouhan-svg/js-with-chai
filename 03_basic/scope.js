0fbc9c8598675166f4de392fea9a7f38d387d01c
Mohit-Chouhan-svg <chouhanmohit247@gmail.com>
2024-12-25 13:50:41 +0000

    scope.js

---
+++ 03_basic/scope.js
@@ -0,0 +1,18 @@
+if(true){
+    const username = "Hitesh";
+    if(true){
+        const website = " youtube";
+        console.log(username + website);
+    }
+}
+console.log("mohit");
+
+console.log(Addone(5));
+
+function Addone(value){
+    return value + 1;
+}
+//console.log(add);
+const add = function Addtwo(value){
+    return value + 2;
+}

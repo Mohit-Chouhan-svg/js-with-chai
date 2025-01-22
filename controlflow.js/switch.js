6bd8d06ff1a5b208fada40841c78148e38ddc0d6
Mohit-Chouhan-svg <chouhanmohit247@gmail.com>
2025-01-22 16:43:00 +0000

    switch

---
+++ controlflow.js/switch.js
@@ -0,0 +1,16 @@
+const month = "march";
+
+switch (month) {
+    case "jan":
+        console.log("jan")
+        break;
+    case "feb":
+        console.log("feb")
+        break;
+    case "march":
+        console.log("march")
+        break;
+
+    default:
+        break;
+}
